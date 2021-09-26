e9038eca9f0d2deaa89af95537eec82e842e806e
mtchou <48898155+mtchou@users.noreply.github.com>
2021-09-26 15:58:13 +1000

    Print of a photo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState(undefined);
+  useEffect( () => {
+    axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-8-30&api_key=DEMO_KEY`)
+    .then(response => {
+    console.log(response);
+    const marsData = response.data.photos;
+    setData(marsData);
+    })
+  }, []);
+
+  console.log('data', data)
+
+  const mostRecentPhotoUrl = data ? data[data.length - 1].img_src : '';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        Mandy React Hello world
+        <img src={mostRecentPhotoUrl} alt=""></img>
+     </div>
   );
 }
 
